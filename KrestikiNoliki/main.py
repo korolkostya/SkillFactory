d1c9b636fc41898936a046bdeb9d375194f78031
korolkostya <korolkostya1999@gmail.com>
2021-06-21 11:38:37 +0300

    Игра крестики нолики

--- KrestikiNoliki/main.py
+++ KrestikiNoliki/main.py
@@ -9,7 +9,7 @@ def play(count):
         print("Ход игрока o:")
     while x>=3 or x<0:
         try:
-            x=int(input("Введите координату строки от 0 до 2:"))
+            x = int(input("Введите координату строки от 0 до 2:"))
         except ValueError:
             print("Необходимо ввести число от 0 до 2!")
         finally:
@@ -37,10 +37,8 @@ def set_value(x, y, v, l):
 def display_matrix(l):
     """Печать матрицы"""
     print("  0 1 2")
-    i = 0
     for i in range(3):
         print(f"{i} {l[i][0]} {l[i][1]} {l[i][2]}")
-        i += 1
 
 
 def victory(l, count):
@@ -53,8 +51,6 @@ def victory(l, count):
         if (l[0][i] == l[1][i] == l[2][i] and l[0][i] != "-") or (l[i][0] == l[i][1] == l[i][2] and l[i][0] != "-"):
             print(f"Победа! Выиграл игрок {v}")
             return False
-        else:
-            i += 1
     if (l[0][0] == l[1][1] == l[2][2] and l[0][0] != "-") or (l[2][0] == l[1][1] == l[0][2] and l[2][0] != "-"):
         print(f"Победа! Выиграл игрок {v}")
         return False
